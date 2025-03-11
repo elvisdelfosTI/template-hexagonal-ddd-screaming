5281b01b5aaea85805165079804deda4f2230f2e
Elvis Mint <elvis.tirado@delfosti.com>
2025-03-11 17:16:03 -0500

    changetest

--- test/config/jest.unit.config.ts
+++ test/config/jest.unit.config.ts
@@ -1,12 +1,15 @@
 import { resolve } from 'node:path';
 import type { Config } from '@jest/types';
 
+const rootDir = resolve(__dirname, '../..');
+
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/../'],
   maxWorkers: 4,
   moduleDirectories: ['node_modules', 'src'],
+  modulePaths: [rootDir],
   cache: true,
   testMatch: ['<rootDir>/../**/*.test.ts'],
   testPathIgnorePatterns: [
@@ -19,7 +22,8 @@ const config: Config.InitialOptions = {
   verbose: true,
   rootDir: '.',
   moduleNameMapper: {
-    '^#(.*)$': '<rootDir>/../src/lib/$1'
+    '^#author/(.*)$': resolve(rootDir, 'src/lib/Author/$1'),
+    '^#(.*)$': resolve(rootDir, 'src/lib/$1')
   },
   transform: {
     '^.+\\.ts$': [
--- test/lib/Book/application/BookEdit/BookEdit.test.ts
+++ test/lib/Book/application/BookEdit/BookEdit.test.ts
@@ -1,34 +1,44 @@
 import { BookEdit } from '../../../../../src/lib/Book/application/UsesCases/BookEdit/BookEdit';
 import { BookGetById } from '../../../../../src/lib/Book/application/UsesCases/BookGetById/BookGetById';
 import { BookSave } from '../../../../../src/lib/Book/application/UsesCases/BookSave/BookSave';
+import type { BookSaveDTO } from '../../../../../src/lib/Book/application/UsesCases/BookSave/BookSaveDTO';
 import { BookId } from '../../../../../src/lib/Book/domain/BookId';
+import type { IBookRepository } from '../../../../../src/lib/Book/domain/BookRepository';
 import { BookStub } from '../../domain/BookStub';
 import { InMemoryBookRepository } from '../../infrastructure/InMemoryBookRepository';
 
 describe('BookEdit', () => {
-	test('should edit a book', async () => {
-		const bookRepository = new InMemoryBookRepository([]);
-		const saveUseCase = new BookSave(bookRepository);
-		const book = BookStub.generateDTO();
-		await saveUseCase.execute(book);
+  let bookRepository: IBookRepository;
+  let saveUseCase: BookSave;
+  let book: BookSaveDTO;
+  let updatedBook: BookSaveDTO;
+  let getByIdUseCase: BookGetById;
+  let editUseCase: BookEdit;
+  beforeAll(() => {
+    bookRepository = new InMemoryBookRepository([]);
+    saveUseCase = new BookSave(bookRepository);
+    book = BookStub.generateDTO();
+    updatedBook = BookStub.generateDTO();
+    getByIdUseCase = new BookGetById(bookRepository);
+    editUseCase = new BookEdit(bookRepository);
+  });
 
-		const bookToEdit = await bookRepository.getById(new BookId(book.id));
-		if (!bookToEdit) throw new Error('Book not found');
-		const editUseCase = new BookEdit(bookRepository);
-		const updatedBook = BookStub.generateDTO();
+  test('should edit a book', async () => {
+    await saveUseCase.execute(book);
+    const bookToEdit = await bookRepository.getById(new BookId(book.id));
+    if (!bookToEdit) throw new Error('Book not found');
 
-		await editUseCase.execute({
-			id: book.id,
-			title: updatedBook.title,
-			publishedDate: updatedBook.publishedDate,
-			pagesCount: updatedBook.pagesCount,
-			ISBN: updatedBook.ISBN,
-			authorId: updatedBook.authorId,
-		});
+    await editUseCase.execute({
+      id: book.id,
+      title: updatedBook.title,
+      publishedDate: updatedBook.publishedDate,
+      pagesCount: updatedBook.pagesCount,
+      ISBN: updatedBook.ISBN,
+      authorId: updatedBook.authorId,
+    });
 
-		const getByIdUseCase = new BookGetById(bookRepository);
-		const editedBook = await getByIdUseCase.execute(book.id);
-		expect(editedBook.title.value).toBe(updatedBook.title);
-		expect(editedBook.ISBN.value).toBe(updatedBook.ISBN);
-	});
+    const editedBook = await getByIdUseCase.execute(book.id);
+    expect(editedBook.title.value).toBe(updatedBook.title);
+    expect(editedBook.ISBN.value).toBe(updatedBook.ISBN);
+  });
 });
