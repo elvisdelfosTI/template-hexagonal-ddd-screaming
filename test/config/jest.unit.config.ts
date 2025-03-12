365798d67eadf0fe6b90f42feae95a40ea78c20b
Elvis Mint <elvis.tirado@delfosti.com>
2025-03-12 15:45:36 -0500

    test1

--- test/config/jest.unit.config.ts
+++ test/config/jest.unit.config.ts
@@ -4,35 +4,37 @@ import type { Config } from '@jest/types';
 const rootDir = resolve(__dirname, '../..');
 
 const config: Config.InitialOptions = {
-	preset: 'ts-jest',
-	testEnvironment: 'node',
-	roots: ['<rootDir>/../'],
-	maxWorkers: 4,
-	moduleDirectories: ['node_modules', 'src'],
-	modulePaths: [rootDir],
-	cache: true,
-	testMatch: ['<rootDir>/../**/*.test.ts'],
-	testPathIgnorePatterns: [
-		'/node_modules/(?!(@ngneat/falso)/)',
-		'/dist/',
-		'.*\\.integration\\.test\\.ts$',
-		'test/lib/.*/infrastructure/.*',
-	],
-	collectCoverage: false,
-	verbose: true,
-	rootDir: '.',
-	moduleNameMapper: {
-		'^#author/(.*)$': resolve(rootDir, 'src/lib/Author/$1'),
-		'^#(.*)$': resolve(rootDir, 'src/lib/$1'),
-	},
-	transform: {
-		'^.+\\.ts$': [
-			'ts-jest',
-			{
-				tsconfig: resolve(__dirname, '../../tsconfig.json'),
-			},
-		],
-	},
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/../'],
+  maxWorkers: 4,
+  moduleDirectories: ['node_modules', 'src'],
+  modulePaths: [rootDir],
+  cache: true,
+  testMatch: ['<rootDir>/../**/*.test.ts'],
+  testPathIgnorePatterns: [
+    '/node_modules/(?!(@ngneat/falso)/)',
+    '/dist/',
+    '.*\\.integration\\.test\\.ts$',
+    'test/lib/.*/infrastructure/.*',
+  ],
+  collectCoverage: true,
+  coverageDirectory: '<rootDir>/../../coverage',
+  coverageReporters: ['text', 'cobertura', 'html'],
+  verbose: true,
+  rootDir: '.',
+  moduleNameMapper: {
+    '^#author/(.*)$': resolve(rootDir, 'src/lib/Author/$1'),
+    '^#(.*)$': resolve(rootDir, 'src/lib/$1'),
+  },
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        tsconfig: resolve(__dirname, '../../tsconfig.json'),
+      },
+    ],
+  },
 };
 
 export default config;
